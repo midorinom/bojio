b562d3f3a80eae69a379f086e6124364c9e97404
Tan Sheng Fong <tanshengfong@Tans-MacBook-Pro.local>
2023-08-28 23:49:26 +0800

    Base project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { AppLayoutComponent } from "./layout/app.layout.component";
+
+// const routes: Routes = [];
+
+@NgModule({
+    // imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) },
+            {
+                // For pages that use the dashboard layout
+                path: '', component: AppLayoutComponent,
+                children: [
+                    // { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                    // { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
+                    // { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+                    // { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
+                    // { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
+                    // { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) }
+                ]
+            },
+            { path: 'notfound', component: NotfoundComponent },
+            { path: '**', redirectTo: '/notfound' },
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
