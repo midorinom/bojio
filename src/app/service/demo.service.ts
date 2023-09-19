92c819a08de066b47a7b0f99ba513b233e006927
Hao Fang <haofang97@gmail.com>
2023-09-20 01:00:03 +0800

    testing url

--- src/app/service/demo.service.ts
+++ src/app/service/demo.service.ts
@@ -8,26 +8,50 @@ export class DemoService {
 
     constructor(private http: HttpClient) { }
 
+    // async getMessages() {
+    //     return await lastValueFrom(this.http.get<any>('http://127.0.0.1:8000/demo/get', { observe: 'response' }))
+    //         .then(response => response)
+    //         .catch(error => error)
+    // }
+
+    // async addMessage(message: DemoMessage) {
+    //     return await lastValueFrom(this.http.post<any>('http://127.0.0.1:8000/demo/add', message, { observe: 'response' }))
+    //         .then(response => response)
+    //         .catch(error => error)
+    // }
+
+    // async updateMessage(message: DemoMessage) {
+    //     return await lastValueFrom(this.http.post<any>('http://127.0.0.1:8000/demo/update', message, { observe: 'response' }))
+    //         .then(response => response)
+    //         .catch(error => error)
+    // }
+
+    // async deleteMessage(message: DemoMessage) {
+    //     return await lastValueFrom(this.http.post<any>('http://127.0.0.1:8000/demo/delete', message, { observe: 'response' }))
+    //         .then(response => response)
+    //         .catch(error => error)
+    // }
+
     async getMessages() {
-        return await lastValueFrom(this.http.get<any>('http://127.0.0.1:8000/demo/get', { observe: 'response' }))
+        return await lastValueFrom(this.http.get<any>('http://3.1.206.9:8000/demo/get', { observe: 'response' }))
             .then(response => response)
             .catch(error => error)
     }
 
     async addMessage(message: DemoMessage) {
-        return await lastValueFrom(this.http.post<any>('http://127.0.0.1:8000/demo/add', message, { observe: 'response' }))
+        return await lastValueFrom(this.http.post<any>('http://3.1.206.9:8000/demo/add', message, { observe: 'response' }))
             .then(response => response)
             .catch(error => error)
     }
 
     async updateMessage(message: DemoMessage) {
-        return await lastValueFrom(this.http.post<any>('http://127.0.0.1:8000/demo/update', message, { observe: 'response' }))
+        return await lastValueFrom(this.http.post<any>('http://3.1.206.9:8000/demo/update', message, { observe: 'response' }))
             .then(response => response)
             .catch(error => error)
     }
 
     async deleteMessage(message: DemoMessage) {
-        return await lastValueFrom(this.http.post<any>('http://127.0.0.1:8000/demo/delete', message, { observe: 'response' }))
+        return await lastValueFrom(this.http.post<any>('http://3.1.206.9:8000/demo/delete', message, { observe: 'response' }))
             .then(response => response)
             .catch(error => error)
     }
