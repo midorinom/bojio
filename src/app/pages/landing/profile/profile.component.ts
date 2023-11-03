9388e004da1af0db7d06fb8d939eba9cba7d58e9
Brandon | Laptop <xinomnom@gmail.com>
2023-11-03 21:50:44 +0800

    remove unnecessary html

--- src/app/pages/landing/profile/profile.component.ts
+++ src/app/pages/landing/profile/profile.component.ts
@@ -14,7 +14,11 @@ export class ProfileComponent {
     password: string = "";
     email: string = "";
 
-    loginUser: User | undefined;
+    loginUser: User= {
+        username: this.username,
+        password: this.password,
+        email: this.email,
+    };
 
     constructor(public layoutService: LayoutService, public router: Router, private accountService: AccountService) { }
 
@@ -36,17 +40,18 @@ export class ProfileComponent {
     }
 
     populateProfileDetails(): void{
-        this.accountService.getProfile().then(response => {
+        this.accountService.getProfile(this.loginUser).then(response => {
             if(response){
-                this.username = response.data.username;
-                this.password = response.data.password;
-                this.email = response.data.email;
+                this.username = response.body.data.username;
+                this.password = response.body.data.password;
+                this.email = response.body.data.email;
             }
         });
     }
 
     editProfile():void{
         let registerUser: User = {
+            user_id: this.loginUser.user_id,
             username: this.username,
             password: this.password,
             email: this.email,
--- src/app/service/account.service.ts
+++ src/app/service/account.service.ts
@@ -28,14 +28,14 @@ export class AccountService {
         .catch(error => error)
     }
 
-    async getProfile(){
-        return await lastValueFrom(this.http.get<any>(ServiceURL + 'profile', { observe: 'response' }))
+    async getProfile(account: User){
+        return await lastValueFrom(this.http.post<any>(ServiceURL + 'profile', account,{ observe: 'response' }))
         .then(response => response)
         .catch(error => error)
     }
 
     async updateProfile(account: User){
-        return await lastValueFrom(this.http.post<any>(ServiceURL + 'profile',account, { observe: 'response' }))
+        return await lastValueFrom(this.http.post<any>(ServiceURL + 'updateprofile',account, { observe: 'response' }))
         .then(response => response)
         .catch(error => error)
     }
