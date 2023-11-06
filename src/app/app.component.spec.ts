bec21d5755190b230675da7d64802ca2a1fe669d
Brandon | Laptop <xinomnom@gmail.com>
2023-11-07 02:00:41 +0800

    fix app.component test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FacadeService } from './service/facade.service';
+import { AccountService } from './service/account.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
+import { ToastModule } from 'primeng/toast';
 
 describe('AppComponent', () => {
     beforeEach(() =>
         TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
+            imports: [RouterTestingModule, ToastModule],
             declarations: [AppComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            providers: [FacadeService, AccountService, HttpClient, HttpHandler, MessageService]
         }),
     );
 
